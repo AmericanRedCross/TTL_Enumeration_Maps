ec3d71d4670b9e00fd7af90f6ad565fdbf173bcb
Robert Banick <robert.banick@redcross.org>
2014-12-11 16:36:27 -0500

    adding shelter_20m

---
+++ shelter_20m/js/20m.js
@@ -0,0 +1,39 @@
+// Create a map in the div #map
+L.mapbox.accessToken = 'pk.eyJ1IjoiYW1lcmljYW5yZWRjcm9zcyIsImEiOiJzdHVRWjA4In0.bnfdwZhKX8tQeMkwY-kknQ';
+
+L.mapbox.map('map','americanredcross.b9f58b6e')
+    .setView([11.0848,124.9269],12);
+
+//lets get barangay.json
+
+$.ajax({
+    type: 'GET',
+    url: 'data/barangays.json',
+    contentType: 'application/json',
+    dataType: 'json',
+    timeout: 10000,
+    success: function(json) {
+        barangays = json;
+        console.log('Success');
+    },
+    error: function(e) {
+        console.log(e);
+        console.log('Error');
+    }
+});
+
+// Switch between barangays
+
+//option for barangays
+//changes location of map to selected barangay centroid (lat/long)
+$('#navigation').change(function() {
+    var place = $('#navigation option:selected').text();
+
+    $.each(barangays, function(index, item) {
+        if (place == item.barangay) {
+            var latlng = L.LatLng(item.lat, item.lon);
+            map.setView(latlng, 17);
+        }
+    });
+
+});
